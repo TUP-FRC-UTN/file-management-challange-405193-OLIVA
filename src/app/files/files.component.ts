bdc548003a9a1ca227b89d70cf632ce7b6abc6d0
405193-OLIVA <405193@tecnicatura.frc.utn.edu.ar>
2024-09-12 11:18:53 -0300

    sort correction

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -10,17 +10,16 @@ import { FileItem, FileType } from '../../models/file.item.model';
 })
 export class FilesComponent {
   @Input() inputFiles:FileItem[] = [];
-  files:FileItem[] = [];
   fileType = FileType.FILE;
 
   ngOnChanges(){
-    this.files = this.inputFiles;
-    this.files.sort((a, b) => {
-      if(a.type < b.type) return -1;
-      if(a.type > b.type) return 1;
-      if(a.name < b.name) return -1;
-      if(a.name > b.name) return -1; 
-      return 0;
+    this.inputFiles = this.inputFiles.sort((a, b) => {
+      if(a.type === b.type){
+        return a.name < b.name ? -1 : 1;
+      }
+      else{
+        return a.type < b.type ? -1 : 1;
+      }
     });
   }
 }
